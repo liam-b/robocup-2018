3720455a314bc49b12ab53e9a76ad71a19a2eae8
Liam Brennan <liam@anywhich.com>
2017-10-11 19:31:14 +1100

    global variables

---
+++ src/gobals.go
@@ -0,0 +1,9 @@
+package main
+
+var DO_KEYBOARD_EVENT = true
+
+var LOOP_SPEED = 5
+
+var COLOR_GREEN_RGB = []int{300, 800, 300} // >
+var COLOR_WHITE_RGB = []int{800, 800, 800} // >
+var COLOR_BLACK_RGB = []int{100, 100, 100} // <
