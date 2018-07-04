3b8320781fb59d6192a83ad091eff8687ff345bf
Liam Brennan <liam@anywhich.com>
2018-07-04 12:18:09 +1000

    #24 hard coded water tower recapture line turn

--- src/behaviours/water_tower.go
+++ src/behaviours/water_tower.go
@@ -8,9 +8,10 @@ const WATER_TOWER_VERIFY_ATTEMPTS = 150
 const WATER_TOWER_VERIFY_SPEED = 40
 const WATER_TOWER_TURN_ANGLE = 900
 const WATER_TOWER_TURN_SPEED = 100
-const WATER_TOWER_AVOID_RATIO = 0.63
+const WATER_TOWER_AVOID_RATIO = 0.65
 const WATER_TOWER_AVOID_SPEED = 300
-const WATER_TOWER_RECAPTURE_ANGLE = 600
+const WATER_TOWER_RECAPTURE_SPEED = 250
+const WATER_TOWER_RECAPTURE_ANGLE = 650
 
 var waterTowerVerifyAttempts = 0
 
@@ -46,6 +47,8 @@ func AvoidWaterTower() string {
   if STATE(":avoid") {
     color, _ :=  GetColors()
     if color == BLACK {
+      go bot.motorRight.RunForever(WATER_TOWER_RECAPTURE_SPEED)
+      go bot.motorLeft.RunForever(-WATER_TOWER_RECAPTURE_SPEED)
       return "water_tower:recapture"
     }
   }
@@ -54,7 +57,7 @@ func AvoidWaterTower() string {
     if GyroTurnedToAngle(WATER_TOWER_RECAPTURE_ANGLE, LEFT) {
       return "follow_line"
     }
-    FollowLine()
+    // FollowLine()
   }
 
   return BEHAVIOUR
