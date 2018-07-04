8fde8a1f55eb15865a8eb33417e30e5b4f656eac
Liam Brennan <liam@anywhich.com>
2018-07-04 12:59:05 +1000

    #8 finalised chemical spill entry

--- src/behaviours/chemical_spill.go
+++ src/behaviours/chemical_spill.go
@@ -1,16 +1,19 @@
 package main
 
 const SAVE_CAN_VERIFY_SPEED = 50
-const SAVE_CAN_VERIFY_ATTEMPTS = 50
+const SAVE_CAN_VERIFY_ATTEMPTS = 30
+const SAVE_CAN_ENTER_SPEED = 300
+const SAVE_CAN_ENTER_COUNT = 60
 
 var chemicalSpillVerifyAttempts = 0
+var chemicalSpillEnterCount = 0
 
 func SaveCan() string {
   if STATE(":start") {
     chemicalSpillVerifyAttempts = 0
     go bot.motorRight.RunForever(SAVE_CAN_VERIFY_SPEED)
     go bot.motorLeft.RunForever(SAVE_CAN_VERIFY_SPEED)
-    return "water_tower:verify"
+    return "chemical_spill:verify"
   }
 
   if STATE(":verify") {
@@ -24,10 +27,24 @@ func SaveCan() string {
 
     left, right := GetColors()
     if left == GREEN && right == GREEN {
-      go bot.motorRight.Stop()
-      go bot.motorLeft.Stop()
+      go bot.motorRight.RunForever(SAVE_CAN_ENTER_SPEED)
+      go bot.motorLeft.RunForever(SAVE_CAN_ENTER_SPEED)
       return "chemical_spill:enter"
     }
+  }
+
+  if STATE(":enter") {
+    chemicalSpillEnterCount += 1
+    if chemicalSpillEnterCount > SAVE_CAN_ENTER_COUNT {
+      chemicalSpillEnterCount = 0
+      go bot.motorRight.Stop()
+      go bot.motorLeft.Stop()
+      return "chemical_spill:search"
+    }
+  }
+
+  if STATE(":search") {
+    // do sum searching
   }
 
   return BEHAVIOUR
